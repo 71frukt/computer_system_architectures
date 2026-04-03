3a1289e836eacbd394f0756eca3553342a140509
71frukt <71frukt@mail.ru>
2026-04-04 00:34:17 +0300

    point 3

--- BranchPredictorAnalysis/no_predict.asm
+++ BranchPredictorAnalysis/no_predict.asm
@@ -4,20 +4,24 @@ main:
     li      a3, 1
     li      a4, 0
     li      a1, 0
+
 loop:
     beq     a4, zero, magic_br_1 # branch #1
     addi    a4, a4, 0
+
+    nop     # 6
+    nop
+    nop
+    nop
+    nop
+    nop
+
 magic_br_1:
     beq     a3, zero, magic_br_2 # branch #2
     addi    a3, a3, 0
+
+
 magic_br_2:
 
-    # ****** ADD HERE ******
-    # your code for task 4
-
-    
-
-    # **********************
-
     addi    a1, a1, 1
-    bne     a1, t0, loop
+    bne     a1, t0, loop         # branch #3
